ad277a6dbbba68218ce1f3bb1ad3aa272b144cbf
Jordan Love <jlove934@uw.edu>
2020-10-05 13:49:17 -0700

    Update StartFrame.java
    Modified blackjack rules and constraints

--- BlackJackSim/src/view/StartFrame.java
+++ BlackJackSim/src/view/StartFrame.java
@@ -31,6 +31,8 @@ public class StartFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public StartFrame() {
+		int i = 0;
+		int blackJackMax = 21;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
